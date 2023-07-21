9b2ee7111adaa279f1326d305da25ea23d613af9
alexey.makhonin <alexey.makhonin@flant.com>
2023-07-21 15:02:46 +0300

    +
    Signed-off-by: alexey.makhonin <alexey.makhonin@flant.com>

--- pkg/master_resolver/master_resolver_test.go
+++ pkg/master_resolver/master_resolver_test.go
@@ -41,7 +41,7 @@ func Test_redisMasterFromSentinelAddr(t *testing.T) {
 		},
 	}
 
-	go mockSentinelServer(&net.TCPAddr{IP: net.IPv4(0, 0, 0, 0), Port: 12700})
+	go mockSentinelServer(mockServerAddr)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := redisMasterFromSentinelAddr(tt.args.sentinelAddress, tt.args.masterName)
